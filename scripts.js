e3873023ad5804bd8cdc2a6a3d6ac3b62e964338
AndersonRadcheski <andersonradcheski@hotmail.com>
2024-06-01 13:19:07 -0300

    primeiro commit mario bross e luigi

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function cliqueinobotao() {
+
+
+    form.style.left = "50%"
+    form.style.transform = "translatex(=50%)"
+    mascara.style.visibility = "visible"
+}
+
+function cliqueinamascara() {
+    form.style.left = "-220px"
+    form.style.transform = "translatex(0)"
+    mascara.style.visibility = "hidden"
+
+
+
+
+
+}
+
+
+
+const form = document.querySelector(".Fale-conosco")
+const mascara = document.querySelector(".mascaraformulario")
